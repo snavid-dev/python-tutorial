48b6e86c800191697e605450ae68faad737b00ac
Sayed Navid <navidjan.azimi132@gmail.com>
2025-12-21 17:47:48 +0430

    some tutorials made

--- .venv/tutorial.py
+++ .venv/tutorial.py
@@ -3,6 +3,14 @@ game = [
     [0, 0, 0],
     [0, 0, 0]
 ]
-print('   a b c')
-for count, row in enumerate(game):
-    print(count, row)
+
+
+def game_board(player=0, row=0, column=0, just_display=False):
+    if not just_display:
+        game[row][column] = player
+    print('   a  b  c')
+    for count, row in enumerate(game):
+        print(count, row)
+
+
+game_board(player=1, row=1, column=1)
