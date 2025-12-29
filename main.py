724683b8f27f4c409fa2ee887afd594f6b55b538
SNA Rahanet <navidjan.azimi132@gmail.com>
2025-12-29 10:48:14 +0430

    made the horizontal winner dynamic

--- main.py
+++ main.py
@@ -21,13 +21,16 @@ def game_board(player=0, row=0, column=0, just_display=False):
 
 def checkWinner(game):
     for row in game:
-        if row[0] == row[1] == row[2] != 0:
-            print(f'Player {row[0]} wins!')
-            stop;
+        matched = True;
+        for column in row:
+            if column != row[0] or column == 0:
+                matched = False
+        if matched:
+            print('Player 1 won!')
+            return
 
 
-
-game_board(player=1, row=2, column=1)
+game_board(player=1, row=1, column=1)
 game_board(player=1, row=2, column=0)
 game_board(player=1, row=2, column=2)
 game_board(just_display=True)
