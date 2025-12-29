ae45b2a30dfbd6c689f392a6a1e9c33c0697fa1e
SNA Rahanet <navidjan.azimi132@gmail.com>
2025-12-29 11:54:24 +0430

    the vertical winner is set and dynamic

--- main.py
+++ main.py
@@ -20,14 +20,25 @@ def game_board(player=0, row=0, column=0, just_display=False):
 
 
 def checkWinner(game):
-    for row in game:
-        if row.count(row[0]) == len(row) and row[0] != 0:
-            print('Player 1 won horizontally!')
+    for col in range(len(game)):
+        check = [];
+        for row in game:
+            check.append(row[col])
+        if check.count(check[0]) == len(check) and check[0] != 0:
+            print(f'Player {check[0]} won vertically!')
             return
 
 
-game_board(player=1, row=2, column=1)
-game_board(player=1, row=2, column=0)
-game_board(player=1, row=2, column=2)
+# Check for winner horizontally
+# def checkWinner(game):
+#     for row in game:
+#         if row.count(row[0]) == len(row) and row[0] != 0:
+#             print('Player 1 won horizontally!')
+#             return
+
+
+game_board(player=2, row=0, column=1)
+game_board(player=2, row=1, column=1)
+game_board(player=2, row=2, column=1)
 game_board(just_display=True)
 checkWinner(game)
